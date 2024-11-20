3359499d9fa5c6d1374ebe11c6fa688149cd5da8
loulan <loulangogogo@163.com>
2024-11-20 15:55:13 +0800

    build: 将 vitest 配置独立为单独的配置文件
    - 从 vite.config.ts 中移除 vitest 测试配置
    - 新建 vitest.config.ts 文件，专门用于 vitest 测试配置
    - 更新测试文件目录路径，使用更通用的匹配模式

--- vite.config.ts
+++ vite.config.ts
@@ -21,15 +21,5 @@ export default defineConfig({
       // iife：立即执行函数，用于直接在浏览器中加载。
       formats: ['es','system','iife','cjs','umd']
     }
-  },
-  // vitest 测试配置
-  test: {
-    // npx vitest --coverage
-    // npx vitest --watch
-    // npx vitest
-    // npx vitest [file]    
-    globals: true,
-    environment: 'node',
-    include: ['test/**/*.test.ts'], // 指定测试文件目录
-  },
+  }
 })
